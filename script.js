f6f71909969a611f39eff53aa3bc6215c2ecb99e
vandana1499 <vandanagupta1499@gmail.com>
2018-08-30 05:08:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+
+
+function myFun()
+{
+    var container=document.getElementsByClassName('container')[0];
+var content1=document.getElementsByClassName('content1')[0];
+
+
+    
+        container.style.display="none";
+     
+
+}
